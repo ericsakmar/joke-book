a816c2285f879ec5bbd3b396212243fc598de589
Eric Sakmar <esakmar@truefit.io>
2021-02-23 11:10:28 -0500

    polling

--- web/src/components/JokesCell/JokesCell.js
+++ web/src/components/JokesCell/JokesCell.js
@@ -16,6 +16,14 @@ export const QUERY = gql`
   }
 `
 
+export const beforeQuery = (props) => {
+  return {
+    variables: props,
+    fetchPolicy: 'network-only',
+    pollInterval: 10 * 1000,
+  }
+}
+
 export const Loading = () => <div>Loading...</div>
 
 export const Empty = () => {
